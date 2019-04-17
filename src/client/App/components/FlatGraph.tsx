b16e3c87a50fa8ae3fc3f2074aa9aec41c72cb4b
efedotov <evgeniy.fedotov@bia-tech.ru>
2019-04-17 15:18:08 +0300

    add list points

--- src/client/App/components/FlatGraph.tsx
+++ src/client/App/components/FlatGraph.tsx
@@ -2,22 +2,47 @@ import React, { Component, ReactElement } from 'react';
 import Canvas from './Canvas';
 import Point from './canvas/Point';
 import Layers from './canvas/Layers';
+import { Position } from './canvas/common';
 
 interface Props {
   children?: ReactElement;
 }
 
+const points: Position[] = [
+  [50, 50],
+  [100, 50],
+  [150, 50],
+  [50, 100],
+  [100, 100],
+  [150, 100],
+  [50, 150],
+  [100, 150],
+  [150, 150],
+];
+
 class FlatGraph extends Component<Props> {
+  onMousemove = (index: number) => () => {
+    console.log(index);
+  };
+
   render = () => {
     return (
       <div className="flat-graph">
         <Canvas>
           <Layers>
-            <Point position={[100, 100]} color="orange" radius={20} />
-            <Point />
-            <Point />
-            <Point />
-            <Point />
+            {points.reduce(
+              (res: any[], position, index) => [
+                ...res,
+                <Point
+                  color="orange"
+                  key={index}
+                  onMousemove={this.onMousemove(index)}
+                  radius={10}
+                  position={position}
+                />,
+              ],
+              [],
+            )}
           </Layers>
         </Canvas>
       </div>
