7979ee1226a08954fb1f3f1a25584d7ef9a5cc5e
efedotov <evgeniy.fedotov@bia-tech.ru>
2019-04-19 09:51:18 +0300

    setup value Point

--- src/client/App/components/FlatGraph.tsx
+++ src/client/App/components/FlatGraph.tsx
@@ -5,24 +5,44 @@ import Layers from './canvas/Layers';
 import { Position } from './canvas/common';
 import Line from './canvas/Line';
 
-interface Props {
-  children?: ReactElement;
+enum PointValue {
+  enabled = 'enabled',
+  active = 'active',
+  disabled = 'disabled',
 }
 
 interface PointConfig {
   position: Position;
   color: string;
-  value: boolean;
   radius: number;
+  value: string;
+  clone: Function;
 }
 
-const getColor = (value: boolean = false) => (value ? 'rebeccapurple' : 'darkorange');
+const getColor = (value?: PointValue): string => {
+  switch (value) {
+    case PointValue.active:
+      return 'rebeccapurple';
+    case PointValue.disabled:
+      return '#E0E0E0';
+    default:
+      return 'darkorange';
+  }
+};
 
-const getRadius = (value: boolean = false) => (value ? 15 : 10);
+const getRadius = (value?: PointValue): number => {
+  switch (value) {
+    case PointValue.active:
+      return 15;
+    default:
+      return 10;
+  }
+};
 
 const createPointConfig = (position: Position = [0, 0]) => (
-  value: boolean = false,
+  value: PointValue = PointValue.enabled,
 ): PointConfig => ({
+  clone: createPointConfig(position),
   radius: getRadius(value),
   color: getColor(value),
   value,
@@ -30,15 +50,15 @@ const createPointConfig = (position: Position = [0, 0]) => (
 });
 
 const defPoints: PointConfig[] = [
-  createPointConfig([50, 50])(true),
+  createPointConfig([50, 50])(PointValue.active),
   createPointConfig([100, 50])(),
-  createPointConfig([150, 50])(true),
+  createPointConfig([150, 50])(PointValue.active),
   createPointConfig([50, 100])(),
-  createPointConfig([100, 100])(true),
+  createPointConfig([100, 100])(PointValue.active),
   createPointConfig([150, 100])(),
-  createPointConfig([50, 150])(true),
+  createPointConfig([50, 150])(PointValue.active),
   createPointConfig([100, 150])(),
-  createPointConfig([150, 150])(true),
+  createPointConfig([150, 150])(PointValue.active),
 ];
 
 type Points = [Position, Position];
@@ -60,6 +80,10 @@ const defLines: LineConfig[] = [
   createLineConfig([[150, 150], [100, 100]]),
 ];
 
+interface Props {
+  children?: ReactElement;
+}
+
 class FlatGraph extends Component<Props> {
   state = {
     points: defPoints,
@@ -71,7 +95,7 @@ class FlatGraph extends Component<Props> {
     const newPoints = [...points];
     const point = points[index];
 
-    newPoints[index] = createPointConfig(point.position)(!point.value);
+    newPoints[index] = createPointConfig(point.position)();
 
     this.setState({ points: newPoints });
   };
